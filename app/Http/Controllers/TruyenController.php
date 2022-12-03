ad0b656d1c71c3f901dd0b3e59d4a74de5ede5b2
cannn <ngoccanscitech@gmail.com>
2022-12-04 05:55:01 +0700

    video 10

--- app/Http/Controllers/TruyenController.php
+++ app/Http/Controllers/TruyenController.php
@@ -13,7 +13,7 @@ class TruyenController extends Controller
      */
     public function index()
     {
-        //
+        return view('admincp.truyen.index');
     }
 
     /**
@@ -23,7 +23,7 @@ class TruyenController extends Controller
      */
     public function create()
     {
-        //
+        return view('admincp.truyen.create');
     }
 
     /**
--- resources/views/admincp/truyen/create.blade.php
+++ resources/views/admincp/truyen/create.blade.php
@@ -8,7 +8,7 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">Thêm danh mục truyện</div>
+                <div class="card-header">Thêm truyện</div>
 
                 <div class="card-body">
                     @if (session('status'))
